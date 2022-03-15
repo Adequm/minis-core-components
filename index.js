dcace32f31488408439cf311b0d8533d1f09457d
Adequm <adequm@bk.ru>
2022-03-16 00:13:29 +0300

    add AppInputFiles to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import AppModal from './components/AppModal.vue';
 import AppTextarea from './components/AppTextarea.vue';
+import AppInputFiles from './components/AppInputFiles.vue';
 import Icon from './components/Icon.vue';
 import SettingsDesktop from './components/SettingsDesktop.vue';
 import SettingsMobile from './components/SettingsMobile.vue';
@@ -8,6 +9,7 @@ import SlideButtons from './components/SlideButtons.vue';
 export {
   AppModal,
   AppTextarea,
+  AppInputFiles,
   Icon,
   SettingsDesktop,
   SettingsMobile,
