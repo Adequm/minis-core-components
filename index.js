d976fcaed75b5c8757f576ae1df6b38c1cf41b86
Adequm <adequm@bk.ru>
2022-03-16 02:06:55 +0300

    add AppLoader

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import AppModal from './components/AppModal.vue';
 import AppTextarea from './components/AppTextarea.vue';
 import AppInputFiles from './components/AppInputFiles.vue';
+import AppLoader from './components/AppLoader.vue';
 import Icon from './components/Icon.vue';
 import SettingsDesktop from './components/SettingsDesktop.vue';
 import SettingsMobile from './components/SettingsMobile.vue';
@@ -10,6 +11,7 @@ export {
   AppModal,
   AppTextarea,
   AppInputFiles,
+  AppLoader,
   Icon,
   SettingsDesktop,
   SettingsMobile,
