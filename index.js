833d762c79bee02372ba8f6649d4c820ff2e9df4
Adequm <adequm@bk.ru>
2022-03-15 22:49:12 +0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+import AppModal from './components/AppModal.vue';
+import AppTextarea from './components/AppTextarea.vue';
+import Icon from './components/Icon.vue';
+import SettingsDesktop from './components/SettingsDesktop.vue';
+import SettingsMobile from './components/SettingsMobile.vue';
+import SlideButtons from './components/SlideButtons.vue';
+
+export {
+  AppModal,
+  AppTextarea,
+  Icon,
+  SettingsDesktop,
+  SettingsMobile,
+  SlideButtons,
+};
